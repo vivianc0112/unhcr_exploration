83efb6b865186e30ca158103b094400c94f4b6f4
Vivian Chien <>
2021-02-17 11:25:24 -0800

    added initial analysis

---
+++ analysis.R
@@ -0,0 +1,8 @@
+# Analysis of UNHCR Data
+
+
+# Set up --------------------------------------------------------------------
+
+library(tidyverse)
+library(dplyr)
+data <- read.csv("data/population.csv")
